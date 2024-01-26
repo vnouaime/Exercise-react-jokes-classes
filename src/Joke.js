a3d4e4660da65134b63aa240054c5766e35ff16a
Vera Nouaime <veranouaime@Veras-MacBook-Air.local>
2024-01-26 14:09:25 -0500

    Completed react-jokes-classes

---
+++ src/Joke.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./Joke.css";
+
+/** A single joke, along with vote up/down buttons. */
+
+function Joke({ vote, votes, text, id }) {
+  function upVote(evt) { vote(id, +1); }
+  function downVote(evt) { vote(id, -1); }
+
+  return (
+    <div className="Joke">
+      <div className="Joke-votearea">
+        <button onClick={upVote}>
+          <i className="fas fa-thumbs-up" />
+        </button>
+
+        <button onClick={downVote}>
+          <i className="fas fa-thumbs-down" />
+        </button>
+
+        {votes}
+      </div>
+
+      <div className="Joke-text">{text}</div>
+    </div>
+  );
+}
+
+export default Joke;
+
+// class Joke extends Component {
+//   render() {
+//     const { id, vote, votes, text } = this.props;
+
+//     return (
+//       <div className="Joke">
+//         <div className="Joke-votearea">
+//           <button onClick={evt => vote(id, +1)}>
+//             <i className="fas fa-thumbs-up" />
+//           </button>
+
+//           <button onClick={evt => vote(id, -1)}>
+//             <i className="fas fa-thumbs-down" />
+//           </button>
+
+//           {votes}
+//         </div>
+
+//         <div className="Joke-text">{text}</div>
+//       </div>
+//     );
+//   }
+// }
+
